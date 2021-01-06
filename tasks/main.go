4f696c1682b0f8792d43467fafa6d889475a7751
zhangqiang <tyokyo320@gmail.com>
2021-01-06 23:39:56 +0900

    update

--- tasks/main.go
+++ tasks/main.go
@@ -2,8 +2,6 @@
 package tasks
 
 import (
-	"fmt"
-
 	"github.com/bamzi/jobrunner"
 )
 
@@ -15,8 +13,8 @@ func RunTasks() {
 	// At 23:00
 	jobrunner.Schedule("TZ=Asia/Tokyo 0 23 * * *", UpdateRate{})
 
-	tasks := jobrunner.Entries()
-	for _, v := range tasks {
-		fmt.Println(v.Job)
-	}
+	// tasks := jobrunner.Entries()
+	// for _, v := range tasks {
+	// 	fmt.Println(v.Job)
+	// }
 }
