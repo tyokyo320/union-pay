ca3331f8da8cade4918135fe42ed4a3e0b9f81c4
zhangqiang <tyokyo320@gmail.com>
2021-01-14 21:32:10 +0900

    update

--- routers/main.go
+++ routers/main.go
@@ -12,8 +12,4 @@ func InitializeRouters(router *gin.Engine) {
 	router.GET("/jobrunner/json", func(c *gin.Context) {
 		c.JSON(200, jobrunner.StatusJson())
 	})
-	router.GET("/jobrunner/html", func(c *gin.Context) {
-		c.HTML(200, "", jobrunner.StatusPage())
-
-	})
 }
