71f16306e3785bd3604a9f4751b4f876a97ff87c
zhangqiang <tyokyo320@gmail.com>
2021-01-14 22:07:05 +0900

    ok

--- tasks/main.go
+++ tasks/main.go
@@ -2,12 +2,6 @@
 package tasks
 
 import (
-	"encoding/json"
-	"fmt"
-	"time"
-	"union-pay/global"
-	"union-pay/repository"
-
 	"github.com/bamzi/jobrunner"
 )
 
@@ -15,45 +9,15 @@ func RunTasks() {
 	// optional: jobrunner.Start(pool int, concurrent int) (10, 1)
 	jobrunner.Start()
 
-	err := jobrunner.Schedule("TZ=Asia/Tokyo */1 * * * *", boloRate{})
-	if err != nil {
-		panic(err)
-	}
-
 	// At minute o past every hour
-	err = jobrunner.Schedule("TZ=Asia/Tokyo 0 * * * *", AddRate{})
+	err := jobrunner.Schedule("0 * * * *", AddRate{})
 	if err != nil {
 		panic(err)
 	}
 
 	// At 23:00
-	err = jobrunner.Schedule("TZ=Asia/Tokyo 0 23 * * *", UpdateRate{})
+	err = jobrunner.Schedule("0 23 * * *", UpdateRate{})
 	if err != nil {
 		panic(err)
 	}
-
-	tasks := jobrunner.Entries()
-	for _, v := range tasks {
-		fmt.Println(v.Job)
-	}
-}
-
-type boloRate struct {
-	// filtered
-}
-
-func (e boloRate) Run() {
-	fmt.Println("run bolo")
-	j, err := json.Marshal(map[string]interface{}{
-		"rate": time.Now().String(),
-		"date": "2020-01-18",
-	})
-	if err != nil {
-		global.ErrorLogger.Println("[tasks add]Json marshal went wrong")
-		return
-	}
-
-	// 并添加至缓存中
-	var redisRepo *repository.RateCacheRepository = repository.NewRateCacheRepository(global.REDIS)
-	redisRepo.Create("latest", string(j))
 }
